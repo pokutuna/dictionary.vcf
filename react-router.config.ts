2e169dc4d95e9b1d5d5b02df39cdb66bd6c18fc2
pokutuna <mail@pokutuna.com>
2025-06-14 18:05:52 +0900

    fix: avoid prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
-  prerender: true,
-  basename: "/dictionary.vcf",
+  // prerender: true,
+  // https://github.com/remix-run/react-router/pull/13791
+  basename: import.meta.env.PROD ? "/dictionary.vcf/" : "/",
 } satisfies Config;
